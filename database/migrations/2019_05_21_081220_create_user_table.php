5638d201a0a7684eb94f41140ba7fbefa6a9fd7f
Fatan <fatankaizen@gmail.com>
2019-05-27 16:33:00 +0700

    New migration

---
+++ database/migrations/2019_05_21_081220_create_user_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // $table->foreign('id')
+            //       ->references('id')
+            //       ->on('category')
+            //       ->onDelete('cascade');
+            $table->string('fullname',30);
+            $table->string('username',15)->unique();
+            $table->string('password');
+            $table->rememberToken();
+            $table->string('email',30)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone',15)->nullable();
+            $table->string('location',30)->nullable();
+            $table->binary('profile_pict')->nullable();
+            $table->string('category',25);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
