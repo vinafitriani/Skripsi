65a57eddd6430b2655da68bfeb175be310e884ea
vinafitriani <vinanurfitriani@gmail.com>
2019-07-11 15:24:44 +0700

    new

--- vind.sql
+++ vind.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2019 at 08:39 AM
+-- Generation Time: Jul 11, 2019 at 10:17 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -25,23 +25,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `category`
+-- Table structure for table `migrations`
 --
 
-CREATE TABLE `category` (
-  `id` int(11) NOT NULL,
-  `category` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `category`
+-- Dumping data for table `migrations`
 --
 
-INSERT INTO `category` (`id`, `category`) VALUES
-(1, 'Model'),
-(2, 'Photographer'),
-(3, 'Fashion Designer'),
-(4, 'Makeup Artist');
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2019_05_21_081220_create_user_table', 1),
+(2, '2019_05_22_065446_create_user_model_table', 1),
+(3, '2019_05_27_062848_create_pict_table', 1),
+(4, '2019_07_03_120134_create_review_table', 1);
 
 -- --------------------------------------------------------
 
@@ -50,66 +51,176 @@ INSERT INTO `category` (`id`, `category`) VALUES
 --
 
 CREATE TABLE `pict` (
-  `username` varchar(15) NOT NULL,
-  `portfolio` mediumblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int(10) UNSIGNED NOT NULL,
+  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `client_file_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `extension` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `size` int(10) UNSIGNED NOT NULL,
+  `mime` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `upload_by` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `dir` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `pict`
+--
+
+INSERT INTO `pict` (`id`, `file_name`, `client_file_name`, `extension`, `size`, `mime`, `upload_by`, `dir`, `created_at`, `updated_at`) VALUES
+(1, '20190710110515_5d25c66be429f7.79368867', 'S__6578342.jpg', 'jpg', 387963, 'image/jpeg', '1', '/portofolio', '2019-07-10 04:05:16', '2019-07-10 04:05:16');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `review`
 --
 
-CREATE TABLE `user` (
-  `fullname` varchar(30) NOT NULL,
-  `username` varchar(15) NOT NULL,
-  `password` varchar(8) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `phone` int(12) NOT NULL,
-  `location` varchar(50) NOT NULL,
-  `profile-pict` mediumblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `review` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `review_user` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_model`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `user_model` (
-  `username` varchar(15) NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `height` int(3) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `fullname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `username` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `location` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profile_pict` blob,
+  `category` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullname`, `username`, `password`, `remember_token`, `email`, `email_verified_at`, `location`, `profile_pict`, `category`, `created_at`, `updated_at`) VALUES
+(1, 'Febrio Cesarianto', 'febrio', '$2y$10$ygERfGOijDHIOhGztUYx/eztx7T828PtHuqHRnP373wIJpWw9UkYm', NULL, 'febriocesarianto@gmail.com', NULL, 'Bandung, Bandung City, West Java, Indonesia', 0x696d616765732f535f5f363537383334322e6a7067, 'model', '2019-07-10 03:44:13', '2019-07-10 04:22:48');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users_model`
+--
+
+CREATE TABLE `users_model` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `gender` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `height` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users_model`
+--
+
+INSERT INTO `users_model` (`id`, `username`, `gender`, `height`, `created_at`, `updated_at`) VALUES
+(1, 'febrio', 'male', 170, NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `category`
+-- Indexes for table `migrations`
 --
-ALTER TABLE `category`
+ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `pict`
 --
 ALTER TABLE `pict`
-  ADD PRIMARY KEY (`username`);
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `user`
+-- Indexes for table `review`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`username`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `review`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `review_user_id_foreign` (`user_id`);
 
 --
--- Indexes for table `user_model`
+-- Indexes for table `users`
 --
-ALTER TABLE `user_model`
-  ADD PRIMARY KEY (`username`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_username_unique` (`username`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- Indexes for table `users_model`
+--
+ALTER TABLE `users_model`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `users_model_username_foreign` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `pict`
+--
+ALTER TABLE `pict`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `review`
+--
+ALTER TABLE `review`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users_model`
+--
+ALTER TABLE `users_model`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `review`
+--
+ALTER TABLE `review`
+  ADD CONSTRAINT `review_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `users_model`
+--
+ALTER TABLE `users_model`
+  ADD CONSTRAINT `users_model_username_foreign` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
